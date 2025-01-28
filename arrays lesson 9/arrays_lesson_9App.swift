24b9afd2a8eb07fe01721ad49c3a22c9bffdc90c
adina778899 <adinamemetea@yahoo.com>
2025-01-27 18:19:09 -0600

    Initial Commit

---
+++ arrays lesson 9/arrays_lesson_9App.swift
@@ -0,0 +1,17 @@
+//
+//  arrays_lesson_9App.swift
+//  arrays lesson 9
+//
+//  Created by natural health on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct arrays_lesson_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
