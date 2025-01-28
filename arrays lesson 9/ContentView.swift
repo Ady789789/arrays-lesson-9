99a4643155ea33e3a44dbe46f519ed554ad24596
adina778899 <adinamemetea@yahoo.com>
2025-01-27 19:50:07 -0600

    arrays in swift w prof Gallaugher

--- arrays lesson 9/ContentView.swift
+++ arrays lesson 9/ContentView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var photoName = ""
+    @State private var photoNumber = 0
+    @State private var messageNumber = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(photoName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 45))
+                .shadow(radius: 50)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+            Spacer()
+            
+            Button("Show Message!") {
+                let messages = ["You are Awesome!",
+                              "You are Great!",
+                              "You are Fantastic!",
+                              "Fabulous? That's You!",
+                                "You  make me Smile!",
+                "When the Genius Bar needs help, they call You!"]
+                message = messages[messageNumber]
+                messageNumber = messageNumber + 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+                
+                
+                photoName = "photo\(photoNumber)"
+//                photoNumber = photoNumber + 1
+                photoNumber += 1
+                if photoNumber > 9 {
+                    photoNumber = 0
+                }
+//        print(photoNumber) do not show on content view only in console
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
+        
         .padding()
     }
 }
